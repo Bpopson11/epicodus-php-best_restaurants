4845519a935dfbf197e058de0b63773a4436eabe
Jason Awbrey and Joeseph Karasek <jason.s.awbrey@gmail.com>
2016-02-24 12:34:12 -0800

    Add functionality for home, cuisine, and restaurant twig pages

--- app/app.php
+++ app/app.php
@@ -5,6 +5,8 @@
 	require_once __DIR__.'/../src/Restaurant.php';
 	require_once __DIR__.'/../src/Review.php';
 
+	use Symfony\Component\Debug\Debug;
+	    Debug::enable();
 
 	$server = 'mysql:host=localhost;dbname=best_rests';
 	$username = 'root';
@@ -12,6 +14,7 @@
 	$DB = new PDO($server, $username, $password);
 
 	$app = new Silex\Application();
+	$app['debug'] = true;
 
 	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
@@ -22,16 +25,45 @@
 
     $app->post("/cuisines", function() use ($app)
 	{
-		$cuisine = new Cuisine($_POST['name']);
+		$cuisine = new Cuisine($_POST['type']);
 		$cuisine->save();
-		return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
+		return $app['twig']->render('home.html.twig', array('cuisines' => Cuisine::getAll()));
 	});
 
 	$app->get("/cuisine/{id}", function($id) use ($app)
 	{
 		$cuisine = Cuisine::find($id);
-		return $app['twig']->render('category.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
+		return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
 	});
+
+
+
+	$app->post("/restaurants", function() use ($app)
+	{
+		$restaurant = new Restaurant($_POST['name'], $_POST['address'], $_POST['cuisine_id'], $_POST['description']);
+		$restaurant->save();
+		$cuisine = Cuisine::find($_POST['cuisine_id']);
+		return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
+
+	});
+
+
+	$app->get("/restaurant/{id}", function($id) use ($app)
+	{
+		$restaurant = Restaurant::find($id);
+		return $app['twig']->render('restaurant.html.twig', array('restaurant' => $restaurant, 'reviews' => $restaurant->getReviews()));
+	});
+
+	$app->post("/reviews", function() use ($app)
+	{
+		$review = new Review($_POST['name'], $_POST['rating'], $_POST['comments'], $_POST['restaurant_id']);
+		$review->save();
+		$restaurant = Restaurant::find($_POST['restaurant_id']);
+		return $app['twig']->render('restaurant.html.twig', array('restaurant' => $restaurant, 'reviews' => $restaurant->getReviews()));
+
+	});
+
+
 
 	return $app;
 
