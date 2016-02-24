46766471bd13ee1cf850ee03ae00889a65363823
Jason Awbrey and Joeseph Karasek <joe@joekarasek.com>
2016-02-24 11:50:36 -0800

    Add home twig page

--- app/app.php
+++ app/app.php
@@ -6,7 +6,6 @@
 	require_once __DIR__.'/../src/Review.php';
 
 
-
 	$server = 'mysql:host=localhost;dbname=best_rests';
 	$username = 'root';
 	$password = 'root';
@@ -16,21 +15,23 @@
 
 	$app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-	$app->get("/", function() use ($app) {
-	        return $app['twig']->render('home.html.twig', array('cuisines' => Cuisine::getAll()));
-	    });
+	$app->get("/", function() use ($app)
+	{
+		return $app['twig']->render('home.html.twig', array('cuisines' => Cuisine::getAll()));
+	});
 
+    $app->post("/cuisines", function() use ($app)
+	{
+		$cuisine = new Cuisine($_POST['name']);
+		$cuisine->save();
+		return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
+	});
 
-	    $app->post("/cuisine", function() use ($app) {
-	      $cuisine = new Cuisine($_POST['name']);
-	      $cuisine->save();
-	      return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
-	  });
-
-	  $app->get("/cuisine/{id}", function($id) use ($app) {
-	      $cuisine = Cuisine::find($id);
-	      return $app['twig']->render('category.html.twig', array('cuisine' => $category, 'restaurants' => $cuisine->getRestaurants()));
-	  });
+	$app->get("/cuisine/{id}", function($id) use ($app)
+	{
+		$cuisine = Cuisine::find($id);
+		return $app['twig']->render('category.html.twig', array('cuisine' => $cuisine, 'restaurants' => $cuisine->getRestaurants()));
+	});
 
 	return $app;
 
